b1d083c0454b5f3706030b68c60efd78dc55a4d2
nguyendiephoang <nguyendiephoang111@gmail.com>
2024-07-07 16:05:20 +0700

    hoang dashboard backend

---
+++ app/src/main/java/com/example/andeee/Domain/Category.java
@@ -0,0 +1,31 @@
+package com.example.andeee.Domain;
+
+public class Category {
+    private int id;
+    private String ImagePath;
+    private String Name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImagePath() {
+        return ImagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        ImagePath = imagePath;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
